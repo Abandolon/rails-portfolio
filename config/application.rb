4af7ff51d1ef28c4b851073b18ee229b3ba73104
Anthony Bandolon <abandolon@gmail.com>
2019-04-02 07:35:18 +0200

    init commit + added postmark configuration

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module RailsPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { :api_token => "858cb095-8107-4845-a788-82d10f95db9a" }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
