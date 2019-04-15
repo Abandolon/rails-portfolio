25c0985b59355b5aae5f4e437f245d9e0eaae5ab
Anthony Bandolon <abandolon@gmail.com>
2019-04-15 15:05:11 +0200

    add contact form to contact section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @skills = Skill.all
     @projects = Project.all
+    @download = Download.new
   end
 end
--- app/mailers/download_mailer.rb
+++ app/mailers/download_mailer.rb
@@ -1,8 +1,16 @@
 class DownloadMailer < ApplicationMailer
 
-  def form_response
-    @user = params[:download]
-
-    mail to: download.email, bcc: "anthony@bandolon.me", subject: "Anthony Bandolon's CV"
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.download_mailer.welcome.subject
+  #
+  def welcome(download)
+    @user = download
+    # attachments['ab_technical_cv.pdf'] = File.read('../assets/images/ab_technical_cv.pdf')
+    mail  to: @user.email,
+          bcc: "anthony@bandolon.me",
+          subject: "Anthony Bandolon's CV"
   end
+
 end
