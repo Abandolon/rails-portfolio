91fc39b91d3b69adad97623ad0393bc6138be2f7
Anthony Bandolon <abandolon@gmail.com>
2019-04-18 12:58:03 +0200

    Added technology references to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_085352) do
+ActiveRecord::Schema.define(version: 2019_04_18_105316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_04_15_085352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.bigint "technology_id"
+    t.index ["technology_id"], name: "index_projects_on_technology_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -39,4 +41,12 @@ ActiveRecord::Schema.define(version: 2019_04_15_085352) do
     t.string "icon"
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "projects", "technologies"
 end
